9d38fe1659293e76996d74a9af3b1432fd03f3f2
Maja <mj.gojska@gmail.com>
2022-04-17 16:37:45 +0200

    nnn

--- src/menu/Ingredient.java
+++ src/menu/Ingredient.java
@@ -1,12 +1,34 @@
 package menu;
 
-public record Ingredient(double price, String name) {
+public class Ingredient {
+
+    private double price;
+    private String name;
+
+    public Ingredient(double price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+
     /**
      * Returns price of the ingredient
      * @return Price of the ingredient
      */
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
      /**
